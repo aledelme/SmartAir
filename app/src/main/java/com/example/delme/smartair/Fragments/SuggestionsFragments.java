b6fb206c53e8efb6a503cdc1f67f593c7e4d9fda
aledelme <alejandrodelmedico@gmail.com>
2018-05-12 16:10:30 +0200

    Primer commit

---
+++ app/src/main/java/com/example/delme/smartair/Fragments/SuggestionsFragments.java
@@ -0,0 +1,30 @@
+package com.example.delme.smartair.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.delme.smartair.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SuggestionsFragments extends Fragment {
+
+
+    public SuggestionsFragments() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_suggestions_fragments, container, false);
+    }
+
+}
