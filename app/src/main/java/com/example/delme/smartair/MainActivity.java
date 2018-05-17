788fc033404b3d2b74d53b54d3eafa3ca5f04743
Andrés <drass.apps@gmail.com>
2018-05-17 10:38:15 +0200

    Remove unnecessary fragment

--- app/src/main/java/com/example/delme/smartair/MainActivity.java
+++ app/src/main/java/com/example/delme/smartair/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.delme.smartair;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,7 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.delme.smartair.Fragments.AboutFragment;
 import com.example.delme.smartair.Fragments.ConsumptionFragment;
 import com.example.delme.smartair.Fragments.OverviewFragment;
 import com.example.delme.smartair.Fragments.PlanesFragment;
@@ -94,9 +90,6 @@ public class MainActivity extends AppCompatActivity
                 break;
             case R.id.nav_suggestions:
                 cambiarFragment(new SuggestionsFragments());
-                break;
-            case R.id.nav_about:
-                cambiarFragment(new AboutFragment());
                 break;
         }
 
