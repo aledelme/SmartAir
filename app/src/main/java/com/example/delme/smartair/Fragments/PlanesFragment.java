20a479616ecdba53c438e8ee5ac1fe2d2ba9687b
Andrés <drass.apps@gmail.com>
2018-05-17 10:21:08 +0200

    Add random values for info data on planes, and remove unnecessary floating button

--- app/src/main/java/com/example/delme/smartair/Fragments/PlanesFragment.java
+++ app/src/main/java/com/example/delme/smartair/Fragments/PlanesFragment.java
@@ -13,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.delme.smartair.R;
 
@@ -22,6 +23,7 @@ public class PlanesFragment extends Fragment implements AdapterView.OnItemSelect
     private Spinner categorieSpinner;
     private ImageView currentPlane;
     private LinearLayout data;
+    private TextView temperature, humdity, quality;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle s) {
@@ -32,9 +34,13 @@ public class PlanesFragment extends Fragment implements AdapterView.OnItemSelect
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        categorieSpinner = getView().findViewById(R.id.planesFragment_spinnerCat);
         data = getView().findViewById(R.id.planesFragment_data);
+        categorieSpinner = getView().findViewById(R.id.planesFragment_spinnerCat);
         currentPlane = getView().findViewById(R.id.planesFragment_planeImageView);
+        temperature = getView().findViewById(R.id.planesFragment_temp);
+        humdity = getView().findViewById(R.id.planesFragment_humidity);
+        quality = getView().findViewById(R.id.planesFragment_quality);
+
         setUpCategories();
         categorieSpinner.setOnItemSelectedListener(this);
     }
@@ -65,27 +71,51 @@ public class PlanesFragment extends Fragment implements AdapterView.OnItemSelect
             switch (parent.getItemAtPosition(pos).toString()){
                 case "General":
                     updateUI("plano");
+                    humdity.setText("40 %HR");
+                    temperature.setText("24 ºC");
+                    quality.setText("95/100");
                     break;
                 case "Salón":
                     updateUI("plano_salon");
+                    humdity.setText("60 %HR");
+                    temperature.setText("25.1 ºC");
+                    quality.setText("90/100");
                     break;
                 case "Cocina":
                     updateUI("plano_cocina");
+                    humdity.setText("40 %HR");
+                    temperature.setText("25.5 ºC");
+                    quality.setText("92/100");
                     break;
                 case "Estudio":
                     updateUI("plano_estudio");
+                    humdity.setText("40 %HR");
+                    temperature.setText("24 ºC");
+                    quality.setText("95/100");
                     break;
                 case "Dormitorio pequeño":
                     updateUI("plano_dormitorio_peq");
+                    humdity.setText("45 %HR");
+                    temperature.setText("24.8 ºC");
+                    quality.setText("95/100");
                     break;
                 case "Dormitorio grande":
                     updateUI("plano_dormitorio_gra");
+                    humdity.setText("45 %HR");
+                    temperature.setText("25 ºC");
+                    quality.setText("95/100");
                     break;
                 case "Baño pequeño":
                     updateUI("plano_aseo_peq");
+                    humdity.setText("50 %HR");
+                    temperature.setText("25 ºC");
+                    quality.setText("93/100");
                     break;
                 case "Baño grande":
                     updateUI("plano_aseo_gra");
+                    humdity.setText("60 %HR");
+                    temperature.setText("25.1 ºC");
+                    quality.setText("92/100");
                     break;
             }
         }
